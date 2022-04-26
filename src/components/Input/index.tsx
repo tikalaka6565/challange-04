41a2a0b3ebca44a3e690210984c2103ee8dc2861
Lok <lucas73641@gmail.com>
2022-04-26 02:29:48 -0300

    projeto finalizado

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,9 +8,16 @@ import {
 import { useField } from '@unform/core';
 
 import { Container } from './styles';
+import { IconBaseProps } from 'react-icons/lib';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface inputProps {
+  name: string,
+  placeholder?: string,
+  icon?: React.ComponentType<IconBaseProps>,
+}
+
+const Input = ({ name, icon: Icon, ...rest }: inputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -48,6 +55,10 @@ const Input = ({ name, icon: Icon, ...rest }) => {
       />
     </Container>
   );
+
 };
 
 export default Input;
+
+
+
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface componentProps {
+  isFocused: boolean,
+  isFilled: boolean,
+}
+
+export const Container = styled.div<componentProps>`
   display: flex;
   align-items: center;
 
